bd2932eff0c9611298542b0ffc29171dff5c8baf
Matheus Cardoso <dacachmatheus@gmail.com>
2023-03-17 14:00:29 -0300

    Add initial version of create partial deck

--- deck/deck.go
+++ deck/deck.go
@@ -2,6 +2,7 @@ package deck
 
 import (
 	"deck_of_cards/card"
+	"fmt"
 	"github.com/google/uuid"
 )
 
@@ -25,4 +26,24 @@ func NewStandardDeck() Deck {
 		Cards:     cards,
 		Remaining: len(cards),
 	}
+}
+
+// deck.go
+
+func NewPartialDeck(codes []string) (Deck, error) {
+	cards := make([]card.Card, 0, len(codes))
+
+	for _, code := range codes {
+		c, err := card.FromString(code)
+		if err != nil {
+			return Deck{}, fmt.Errorf("invalid card code '%s': %w", code, err)
+		}
+		cards = append(cards, c)
+	}
+
+	return Deck{
+		ID:        uuid.New(),
+		Cards:     cards,
+		Remaining: len(cards),
+	}, nil
 }
--- deck/deck_test.go
+++ deck/deck_test.go
@@ -26,3 +26,20 @@ func TestNewStandardDeck(t *testing.T) {
 		}
 	}
 }
+
+func TestNewPartialDeckCards(t *testing.T) {
+	codes := []string{"AS", "KD", "AC", "2C", "KH"}
+
+	deck, err := NewPartialDeck(codes)
+
+	assert.NoError(t, err, "Creating a partial deck with example codes should not return an error")
+	// Relevant fields are populated.
+	assert.NotNil(t, deck.ID, "Deck ID should not be nil")
+	assert.Len(t, deck.Cards, len(codes), "Partial deck should have the specified number of cards")
+	assert.Equal(t, len(codes), deck.Remaining, "Remaining cards should match the number of input codes")
+
+	// The cards we passed by code are the deck's cards.
+	for i, code := range codes {
+		assert.Equal(t, code, deck.Cards[i].String(), "Deck card should match the specified code")
+	}
+}
