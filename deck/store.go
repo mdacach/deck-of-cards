8088ca71918f5b8674ed29966d737840c1d9544d
Matheus Cardoso <dacachmatheus@gmail.com>
2023-03-18 18:26:04 -0300

    Add documentation for Store

--- deck/store.go
+++ deck/store.go
@@ -6,25 +6,30 @@ import (
 	"sync"
 )
 
+// Store manages a collection of decks in a thread-safe manner. Decks are accessed by their ID (uuid).
 type Store struct {
 	decks map[uuid.UUID]*Deck
 	// Maps are not safe for concurrent access, so we will synchronize with a Mutex.
-	// It is OK to have concurrent reads though, se we will RWMutex.
+	// It is OK to have concurrent reads though, se we use RWMutex.
 	mu sync.RWMutex
 }
 
+// NewStore creates and returns a new instance of the Store type.
 func NewStore() *Store {
 	return &Store{
 		decks: make(map[uuid.UUID]*Deck),
 	}
 }
 
+// Add adds a new deck to the store. It returns an error if a deck with the same ID already exists in the store.
 func (s *Store) Add(deck *Deck) error {
 	s.mu.Lock()
 	// From Go 1.14 we don't need to worry about performance of defer here.
 	// Source: https: //www.reddit.com/r/golang/comments/fdy6sb/comment/fjl6d37/?utm_source=share&utm_medium=web2x&context=3
 	defer s.mu.Unlock()
 
+	// It is better to return an error here then to overwrite a deck. The overwritten deck may have been used.
+	// Or overwriting decks could be a potential attack.
 	if _, exists := s.decks[deck.ID]; exists {
 		return errors.New("deck ID already exists in the store")
 	}
@@ -33,6 +38,7 @@ func (s *Store) Add(deck *Deck) error {
 	return nil
 }
 
+// Get retrieves a deck from the store by its ID. It returns the deck and nil if the deck is found, or nil and an error if the deck is not found.
 func (s *Store) Get(id uuid.UUID) (*Deck, error) {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
@@ -42,6 +48,7 @@ func (s *Store) Get(id uuid.UUID) (*Deck, error) {
 	return nil, errors.New("deck not found")
 }
 
+// Remove removes a deck from the store by its ID. It returns an error if the deck is not found.
 func (s *Store) Remove(deckID uuid.UUID) error {
 	s.mu.Lock()
 	defer s.mu.Unlock()
