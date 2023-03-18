fe3e36b3e89a3d7fc790bebfc6d79ffafd545b33
Matheus Cardoso <dacachmatheus@gmail.com>
2023-03-18 17:59:46 -0300

    Add documentation for Suit

--- card/suit.go
+++ card/suit.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 )
 
+// Suit represents the suit of a playing card (Spades, Diamonds, Clubs, Hearts).
 type Suit string
 
 const (
@@ -14,10 +15,12 @@ const (
 	Hearts   Suit = "H"
 )
 
+// Suits returns a slice of all valid Suit values, in order (Spades, Diamonds, Clubs, Hearts).
 func Suits() []Suit {
 	return []Suit{Spades, Diamonds, Clubs, Hearts}
 }
 
+// IsValid checks whether the Suit is a valid value.
 // TODO: This will be refactored. The validation will happen in the Rank constructor.
 func (s Suit) IsValid() bool {
 	for _, validSuit := range Suits() {
@@ -28,6 +31,7 @@ func (s Suit) IsValid() bool {
 	return false
 }
 
+// LongString returns the long form string representation of the Suit (e.g., "SPADES", "DIAMONDS", "CLUBS", "HEARTS").
 func (s Suit) LongString() string {
 	switch s {
 	case Spades:
@@ -44,6 +48,8 @@ func (s Suit) LongString() string {
 	}
 }
 
+// ParseLongSuit takes a long form suit string and returns the corresponding Suit value.
+// It returns an error if the input string is not a valid long form suit.
 func ParseLongSuit(s string) (Suit, error) {
 	switch strings.ToUpper(s) {
 	case "SPADES":
