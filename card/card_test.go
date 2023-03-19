f383b5611e8688153c6860184651bedf9ae2216f
Matheus Cardoso <dacachmatheus@gmail.com>
2023-03-19 12:38:57 -0300

    Standardize tests

--- card/card_test.go
+++ card/card_test.go
@@ -2,46 +2,47 @@ package card
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 // TODO: These tests could be improved by using property-based testing.
 func TestCardString(t *testing.T) {
-	cases := []struct {
+	testCases := []struct {
+		name     string
 		card     Card
 		expected string
 	}{
-		{card: Card{Suit: Spades, Rank: Ace}, expected: "AS"},
-		{card: Card{Suit: Hearts, Rank: Ten}, expected: "10H"},
-		{card: Card{Suit: Diamonds, Rank: Queen}, expected: "QD"},
-		{card: Card{Suit: Clubs, Rank: Jack}, expected: "JC"},
+		{"Ace of Spades -> AS", Card{Suit: Spades, Rank: Ace}, "AS"},
+		{"Ten of Hearts -> 10H", Card{Suit: Hearts, Rank: Ten}, "10H"},
+		{"Queen of Diamonds -> QD", Card{Suit: Diamonds, Rank: Queen}, "QD"},
+		{"Jack of Clubs -> JC", Card{Suit: Clubs, Rank: Jack}, "JC"},
 	}
 
-	for _, test := range cases {
-		code := test.card.String()
-		if code != test.expected {
-			t.Errorf("Expected   code %q, but got %q", test.expected, code)
-		}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			code := tc.card.String()
+			assert.Equal(t, tc.expected, code, "Expected code %q, got: %q", tc.expected, code)
+		})
 	}
 }
 
 func TestCardFromStringValid(t *testing.T) {
 	testCases := []struct {
+		name         string
 		input        string
 		expectedCard Card
 	}{
-		{"AS", Card{Rank: Ace, Suit: Spades}},
-		{"KD", Card{Rank: King, Suit: Diamonds}},
-		{"5H", Card{Rank: Five, Suit: Hearts}},
-		{"9C", Card{Rank: Nine, Suit: Clubs}},
-		{"2S", Card{Rank: Two, Suit: Spades}},
-		{"10S", Card{Rank: Ten, Suit: Spades}},
+		{"AS -> Ace of Spades", "AS", Card{Rank: Ace, Suit: Spades}},
+		{"KD -> King of Diamonds", "KD", Card{Rank: King, Suit: Diamonds}},
+		{"5H -> Five of Hearts", "5H", Card{Rank: Five, Suit: Hearts}},
+		{"9C -> Nine of Clubs", "9C", Card{Rank: Nine, Suit: Clubs}},
+		{"2S -> Two of Spades", "2S", Card{Rank: Two, Suit: Spades}},
+		{"10S -> Ten of Spades", "10S", Card{Rank: Ten, Suit: Spades}},
 	}
 
 	for _, tc := range testCases {
-		t.Run(fmt.Sprintf("FromString_%s", tc.input), func(t *testing.T) {
+		t.Run(tc.name, func(t *testing.T) {
 			card, err := FromString(tc.input)
 			assert.NoError(t, err)
 			assert.Equal(t, tc.expectedCard, card)
@@ -72,41 +73,49 @@ func TestCardFromStringInvalid(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			_, err := FromString(tc.input)
-			t.Logf("error: %s", err)
-			assert.Error(t, err)
+			assert.Error(t, err, "Invalid card code should return an error")
 		})
 	}
 }
 
 func TestCardMarshalJSON(t *testing.T) {
 	testCases := []struct {
+		name         string
 		card         Card
 		expectedJSON string
 	}{
-		{Card{Rank: Queen, Suit: Hearts}, `{"value":"QUEEN","suit":"HEARTS","code":"QH"}`},
-		{Card{Rank: Ace, Suit: Spades}, `{"value":"ACE","suit":"SPADES","code":"AS"}`},
+		{"Queen of Hearts", Card{Rank: Queen, Suit: Hearts}, `{"value":"QUEEN","suit":"HEARTS","code":"QH"}`},
+		{"Ace of Spades", Card{Rank: Ace, Suit: Spades}, `{"value":"ACE","suit":"SPADES","code":"AS"}`},
+		{"Ten of Spades", Card{Rank: Ten, Suit: Spades}, `{"value":"TEN","suit":"SPADES","code":"10S"}`},
 	}
 
 	for _, tc := range testCases {
-		jsonData, err := json.Marshal(tc.card)
-		assert.NoError(t, err, "Error marshaling card")
-		assert.JSONEq(t, tc.expectedJSON, string(jsonData), "Card JSON representation is not as expected")
+		t.Run(tc.name, func(t *testing.T) {
+			jsonData, err := json.Marshal(tc.card)
+			assert.NoError(t, err, "Error marshalling card")
+			assert.JSONEq(t, tc.expectedJSON, string(jsonData), "Card JSON representation is not as expected")
+		})
 	}
 }
 
 func TestCardUnmarshalJSON(t *testing.T) {
 	testCases := []struct {
+		name         string
 		inputJSON    string
 		expectedCard Card
 	}{
-		{`{"value":"QUEEN","suit":"HEARTS","code":"QH"}`, Card{Rank: Queen, Suit: Hearts}},
-		{`{"value":"ACE","suit":"SPADES","code":"AS"}`, Card{Rank: Ace, Suit: Spades}},
+		{"JSON -> Queen of Hearts", `{"value":"QUEEN","suit":"HEARTS","code":"QH"}`, Card{Rank: Queen, Suit: Hearts}},
+		{"JSON -> Ace of Spades", `{"value":"ACE","suit":"SPADES","code":"AS"}`, Card{Rank: Ace, Suit: Spades}},
+		{"JSON -> Ten of Clubs", `{"value":"TEN","suit":"CLUBS","code":"10"}`, Card{Rank: Ten, Suit: Clubs}},
 	}
 
 	for _, tc := range testCases {
-		var c Card
-		err := json.Unmarshal([]byte(tc.inputJSON), &c)
-		assert.NoError(t, err, "Error unmarshalling card")
-		assert.Equal(t, tc.expectedCard, c, "Unmarshalled card is not as expected")
+		t.Run(tc.name, func(t *testing.T) {
+			var c Card
+			err := json.Unmarshal([]byte(tc.inputJSON), &c)
+
+			assert.NoError(t, err, "Error unmarshalling card")
+			assert.Equal(t, tc.expectedCard, c, "Unmarshalled card is not as expected")
+		})
 	}
 }
